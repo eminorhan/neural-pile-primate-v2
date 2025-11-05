f9cf3a59c52d1c73916d8cb4215e3cd7048ca34f
Emin Orhan <aeminorhan@gmail.com>
2025-11-05 23:00:38 +0000

    add extract_motifs

--- download_dataset.py
+++ download_dataset.py
@@ -1,4 +1,12 @@
+import numpy as np
 from datasets import load_dataset
 
-ds = load_dataset("eminorhan/neural-pile-primate", num_proc=32, split='train')
-print(ds[0])
+ds = load_dataset("eminorhan/neural-pile-primate", num_proc=32)
+print(f"Number of data rows in train: {len(ds['train'])}")
+print(f"Number of data rows in test: {len(ds['test'])}")
+
+for i in range(9):
+    spike_counts = np.array(ds['train'][i]['spike_counts'])
+    print(f"Data row {i} shape: {spike_counts.shape}")
+
+print(f"Done!")
